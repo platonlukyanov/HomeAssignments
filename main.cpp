1ebf42ee24894c1711e6e87e3cedab27a7d4ae36
Platon Lukyanov <st128133@student.spbu.ru>
2024-09-15 14:29:14 +0300

    Implemented saying hello not only to the world, but to any string passed in the loop (can also be stopped)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 #include "hello.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    sayHi("world");
+    string userWord = "world";
+
+    do {
+	sayHi(userWord);
+    	cin >> userWord;
+    } while (userWord != "stop");    
 }
 
