4cd7f879f0c228a93b3f13c19d62004b9c35ccd6
Platon Lukyanov <st128133@student.spbu.ru>
2024-09-15 14:40:52 +0300

    Tailored .gitignore to the project specific, added required file comments

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,6 @@
+/* Platon Lukyanov st128133@student.spbu.ru
+ * Problem 1
+ */
 #include "hello.h"
 #include <iostream>
 using namespace std;
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+/* Platon Lukyanov st128133@student.spbu.ru
+ * Problem 1
+*/
 #include <iostream>
 #include <string>
 #include "hello.h"
