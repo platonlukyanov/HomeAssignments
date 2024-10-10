843811c1f118f152632d0216d65e0ee26fa0094c
Platon Lukyanov <st128133@student.spbu.ru>
2024-10-10 23:31:38 +0300

    Completed 2b

---
+++ HomeAssignment2b/main.cpp
@@ -0,0 +1,72 @@
+/* Platon Lukyanov st128133@student.spbu.ru
+ * Problem 2b
+*/
+#include <iostream>
+#include <cctype>
+
+template <typename T>
+class Stack {
+    private:
+        T* arr;
+        int capacity;
+        int top;
+    public:
+        Stack(int size) {
+            arr = new T[size];
+            capacity = size;
+            top = -1;
+        }
+
+        void push(T x) {
+            if (top == capacity - 1) {
+                std::cerr << "Stack overflow!\n";
+                return;
+            }
+
+            arr[++top] = x;
+        }
+
+        T pop() {
+            if (top == -1) {
+                std::cerr << "Stack is empty!\n";
+            }
+
+            T toBePopped = arr[top];
+            top--;
+            return toBePopped;
+        }
+
+        ~Stack() {
+            delete[] arr;
+        }
+};
+
+int main(int argc, char *argv[]) {
+    int number_of_operations = std::stoi(argv[1]);
+    Stack<int> stack(2); // we won't need more than 2 elements on stack for binary operations
+    std::string userInput;
+
+    for (int i = 0; i < number_of_operations; i++) {
+        std::cin >> userInput;
+        if (userInput == "+") {
+            stack.push(stack.pop() + stack.pop());
+        }
+        else if (userInput == "-") {
+            stack.push(stack.pop() - stack.pop());
+        }
+        else if (userInput == "*") {
+            stack.push(stack.pop() * stack.pop());
+        }
+        else if (userInput == "/") {
+            int b = stack.pop();
+            int a = stack.pop();
+            stack.push(a / b);
+        } else {
+            stack.push(std::stoi(userInput));
+        }
+    }
+
+    std::cout << stack.pop() << '\n'; 
+
+    return 0;
+}
